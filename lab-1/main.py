89c4e041582ef2f60119122f77acb1acc42c4b67
Soff4 <sofa00sofa@gmail.com>
2024-04-16 19:44:35 +0300

    upload modified lab

--- lab-1/main.py
+++ lab-1/main.py
@@ -3,18 +3,16 @@ import sys
 def prime_checker(num):
     try:
         num = int(num)
-        if num <= 1:
+        if num <= 0:
             sys.stdout.write("False")
-            sys.exit(1)
+            sys.exit(0)
         else:
             for i in range(2, num):
                 if (num % i) == 0:
                     sys.stdout.write("False")
-                    sys.exit(1)
+                    sys.exit(0)
             sys.stdout.write("True")
             sys.exit(0)
-    except (ValueError, TypeError):
+    except (TypeError, ValueError):
         sys.stderr.write(f"Error: Введене значення '{num}' не є цілим числом.\n")
         sys.exit(1)
-
-...
--- lab-1/test.py
+++ lab-1/test.py
@@ -1,4 +1,3 @@
-import sys
 import unittest
 from main import prime_checker
 
@@ -6,11 +5,19 @@ class TestPrimeChecker(unittest.TestCase):
     def test_prime_checker(self):
         with self.assertRaises(SystemExit) as cm:
             prime_checker(4)
-        self.assertEqual(cm.exception.code, 1)
+            self.assertEqual(cm.exception.code, 0)
 
         with self.assertRaises(SystemExit) as cm:
-            prime_checker(17)
-        self.assertEqual(cm.exception.code, 0)
+            prime_checker(97)
+            self.assertEqual(cm.exception.code, 0)
+        
+        with self.assertRaises(SystemExit) as cm:
+            prime_checker("hello")
+            self.assertEqual(cm.exception.code, 1)
+        
+        with self.assertRaises(SystemExit) as cm:
+            prime_checker("97")
+            self.assertEqual(cm.exception.code, 1)
 
 if __name__ == '__main__':
     unittest.main()
